d96737ed3ac3ae732a12293212b39b577c7b9fbe
Raul <100451269@alumnos.uc3m.es>
2022-12-26 02:16:36 +0100

    day 12 started

--- 2022/12.py
+++ 2022/12.py
@@ -2,30 +2,14 @@ import sys
 from Inputs.ReadFile import *
 import queue
 
-class Node:
-	def __init__(self, value, i , j):
-		self.id = (i, j)
-		self.value = value
-		self.posib = []
-
-	# operator overloading for <, >, <=, >=, ==, !=
-	def __lt__(self, other):
-		return ord(self.value) < ord(other.value)
-	def __gt__(self, other):
-		return ord(self.value) > ord(other.value)
-	def __le__(self, other):
-		return ord(self.value) <= ord(other.value)
-	def __ge__(self, other):
-		return ord(self.value) >= ord(other.value)
-	def __eq__(self, other):
-		return ord(self.value) == ord(other.value)
-	def __ne__(self, other):
-		return ord(self.value) != ord(other.value)
-
 class Problem:
 	def __init__(self, inp, part):
 		self.inp = inp
 
+		self.d = {a: ord(a) - 96 for a in 'abcdefghijklmnopqrstuvwxyz'}
+		self.d['S'] = 1
+		self.d['E'] = 26
+		self.dirs = ((0, 1), (0, -1), (1, 0), (-1, 0))
 		if part == '1':
 			print(self.part1())
 		else:
@@ -34,57 +18,32 @@ class Problem:
 	def create_nodes(self):
 		Start = None
 		End = None
-		M = self.inp
-		nodes = []
-		for i in range(len(M)):
-			nodes.append([])
-			for j in range(len(M[0])):
-				if M[i][j] == 'S':
-					new_node = Node(M[i][j], i, j)
-					Start = new_node
-				elif M[i][j] == 'E':
-					new_node = Node(M[i][j], i, j)
-					End = new_node
-				else:
-					new_node = Node(M[i][j], i, j)
-				nodes[i].append(new_node)	
-		for i in range(len(M)):
-			for j in range(len(M[0])):
-				if i > 0:
-					if self.can_go(nodes[i][j].value, nodes[i-1][j].value):
-						nodes[i][j].posib.append(nodes[i-1][j])
-				if i < len(M) - 1:
-					if self.can_go(nodes[i][j].value, nodes[i+1][j].value):
-						nodes[i][j].posib.append(nodes[i+1][j])
-				if j > 0:
-					if self.can_go(nodes[i][j].value, nodes[i][j-1].value):
-						nodes[i][j].posib.append(nodes[i][j-1])
-				if j < len(M[0]) - 1:
-					if self.can_go(nodes[i][j].value, nodes[i][j+1].value):
-						nodes[i][j].posib.append(nodes[i][j+1])
+		for i in range(len(self.inp)):
+			for j in range(len(self.inp[0])):
+				if self.inp[i][j] == 'S':
+					Start = (i, j)
+				elif self.inp[i][j] == 'E':
+					End = (i, j)
 		return Start, End
-		
-	def dijkstra(self, Start, End):
-		q = queue.PriorityQueue()
-		q.put((0, Start))
+
+	def bfs(self, Start, End):
+		q = deque()
+		q.put(Start)
 		visited = set()
+		visited.add(Start.id)
 		while not q.empty():
 			current = q.get()
-			if current[1] == End:
-				return current[0]
-			if current[1].id not in visited:
-				visited.add(current[1].id)
-				for node in current[1].posib:
-					if node.id not in visited:
-						q.put((current[0] + 1, node))
-		return -1
-
-	def can_go(self, current, next):
-		return abs(ord(current) - ord(next)) <= 1
+			if current == End:
+				return True
+			for node in current.posib:
+				if node.id not in visited:
+					q.put(node)
+					visited.add(node.id)
+		return False
 
 	def part1(self):
 		Start, End = self.create_nodes()
-		return self.dijkstra(Start, End)
+		return self.bfs(Start, End)
 
 	def part2(self):
 		pass
